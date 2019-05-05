4e36cafebb7a5958a8f0cebab51cbde7e9ae5562
Oleksandr Dodatko <dodikk88.reg@gmail.com>
2019-05-05 16:51:56 +0300

    cleanup in main index.js file. extracted unrelated code to samples.js file

---
+++ 2-samples.js
@@ -0,0 +1,53 @@
+global.fetch = require("node-fetch");
+const main = require("async-main").default;
+const Exchange = require("./exchange.js");
+const Indicators = require("./indicators.js");
+const Logger = require("./logger.js");
+
+
+// ==== moving average indicator usage
+// -
+const DEFAULT_WINDOW_SIZE = 100;
+
+async function MovingAverageIndicatorsLogicAsync()
+{
+	const nowMovingAverageDay =
+	await Indicators.DailyMovingAverageAsync(
+		/*cryptoAsset:*/ "BTC",
+		/*currency:*/ "USD",
+		/*hours:*/ DEFAULT_WINDOW_SIZE,
+		/*currentElementIndex:*/ 0
+	);
+
+	console.log('daily 100 nowMovingAverage : ' + nowMovingAverageDay);
+
+	const nowMovingAverageHour =
+	await Indicators.HourlyMovingAverageAsync(
+		/*cryptoAsset:*/ "BTC",
+		/*currency:*/ "USD",
+		/*hours:*/ DEFAULT_WINDOW_SIZE,
+		/*currentElementIndex:*/ 0
+	);
+
+	console.log('hourly 100 nowMovingAverage : ' + nowMovingAverageHour);
+
+	const nowMovingAverageMinute =
+	await Indicators.MinutelyMovingAverageAsync(
+		/*cryptoAsset:*/ "BTC",
+		/*currency:*/ "USD",
+		/*hours:*/ DEFAULT_WINDOW_SIZE,
+		/*currentElementIndex:*/ 0
+	);
+
+	console.log('minutely 100 nowMovingAverage : ' + nowMovingAverageMinute);
+}
+
+
+// ==== market orders usage
+// -
+async function MarketOrdersLogicAsync()
+{
+	await Exchange.MarketBuyBitcoinAsync(1);
+	await Exchange.MarketSellBitcoinAsync(1);
+}
+
--- index.js
+++ index.js
@@ -6,54 +6,6 @@ const Indicators = require("./indicators.js");
 const Logger = require("./logger.js");
 
 
-// ==== moving average indicator usage
-// -
-const DEFAULT_WINDOW_SIZE = 100;
-
-async function MovingAverageIndicatorsLogicAsync()
-{
-	const nowMovingAverageDay =
-	await Indicators.DailyMovingAverageAsync(
-		/*cryptoAsset:*/ "BTC",
-		/*currency:*/ "USD",
-		/*hours:*/ DEFAULT_WINDOW_SIZE,
-		/*currentElementIndex:*/ 0
-	);
-
-	console.log('daily 100 nowMovingAverage : ' + nowMovingAverageDay);
-
-	const nowMovingAverageHour =
-	await Indicators.HourlyMovingAverageAsync(
-		/*cryptoAsset:*/ "BTC",
-		/*currency:*/ "USD",
-		/*hours:*/ DEFAULT_WINDOW_SIZE,
-		/*currentElementIndex:*/ 0
-	);
-
-	console.log('hourly 100 nowMovingAverage : ' + nowMovingAverageHour);
-
-	const nowMovingAverageMinute =
-	await Indicators.MinutelyMovingAverageAsync(
-		/*cryptoAsset:*/ "BTC",
-		/*currency:*/ "USD",
-		/*hours:*/ DEFAULT_WINDOW_SIZE,
-		/*currentElementIndex:*/ 0
-	);
-
-	console.log('minutely 100 nowMovingAverage : ' + nowMovingAverageMinute);
-}
-
-
-// ==== market orders usage
-// -
-async function MarketOrdersLogicAsync()
-{
-	await Exchange.MarketBuyBitcoinAsync(1);
-	await Exchange.MarketSellBitcoinAsync(1);
-}
-
-// === simple strategy
-// -
 var hasPosition = false;
 
 async function MovingAverageStrategyTick()
